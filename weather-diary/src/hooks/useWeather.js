a7fa02fc194b43c75a4494cf0f864205429b809f
Yunseok <watemark1017@gmail.com>
2025-04-01 20:25:21 +0900

    feat: OpenWeatherAPI 연결

--- weather-diary/src/hooks/useWeather.js
+++ weather-diary/src/hooks/useWeather.js
@@ -2,19 +2,25 @@ import { useEffect, useState } from "react";
 
 function useWeather() {
   const [weather, setWeather] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (pos) => {
       const { latitude, longitude } = pos.coords;
-      const apiKey = "YOUR_API_KEY";
+      const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
       const res = await fetch(url);
       const data = await res.json();
       setWeather(data.weather[0].main + " / " + data.main.temp + "°C");
+      setLoading(false);
+    }, (err) => {
+      setError(err);
+      setLoading(false);
     });
   }, []);
 
-  return weather;
+  return { weather, loading, error };
 }
 
 export default useWeather;
--- weather-diary/src/pages/Write.jsx
+++ weather-diary/src/pages/Write.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { saveDiary, loadDiary } from "@/utils/storage";
+import useWeather from "../hooks/useWeather";
+
 import cn from "@/utils/cn";
 
 function Write() {
@@ -9,42 +11,66 @@ function Write() {
 
   const navigate = useNavigate();
   let diaryList = loadDiary();
+  const { weather } = useWeather();
 
   const handleCreateDiary = () => {
+    if (!title || !content) {
+      alert("제목과 내용을 모두 입력해주세요.");
+      return;
+    }
+
     const diary = {
       title,
       content,
       date: new Date().toISOString().split('T')[0],
-      weather: "sunny", // 기본값으로 sunny 설정
+      weather: weather,
     }
-    saveDiary([...diaryList, diary]);
+    saveDiary([ diary, ...diaryList]);
     navigate('/');
   }
 
   return(
-    <div className="flex flex-col h-screen">
-      <h1 className="text-2xl font-bold">✍️ 일기 작성 페이지</h1>
-      <input 
-        placeholder="제목을 작성하세요."
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className="mt-4 p-2 border rounded"
-        type="text"
-      />
-      <textarea 
-        placeholder="일기를 작성하세요."
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        className="mt-4 p-2 border rounded resize-none"
-      />
+    <div className="flex flex-col h-screen w-full max-w-xl items-center">
+      <h1 className="text-2xl font-bold mt-6">✍️ 일기 작성 페이지</h1>
+      <section className="flex flex-col items-center mt-4 w-full">
+        <div className="flex items-center justify-between w-full mt-4">
+          <p className="text-xl font-semibold">날씨: {weather}</p>
+          {/* <img src={icon} alt="weather icon" className="w-12 h-12" /> */}
+        </div>
+      </section>
+      <section className="flex flex-col items-center mt-4 w-full">
+        <p className="text-xl mt-2 w-full font-semibold">제목</p>
+        <input 
+          placeholder="제목을 작성하세요."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className={cn(
+            "mt-4 p-2 border rounded w-full bg-white",
+            "focus:outline-none border-gray-600",
+          )}
+          type="text"
+        />
+      </section>
+      <section className="flex flex-col items-center mt-4 w-full">
+        <p className="text-xl mt-2 w-full font-semibold">내용</p>
+        <textarea 
+          placeholder="일기를 작성하세요."
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className={cn(
+            "mt-4 p-2 border rounded w-full bg-white",
+            "focus:outline-none border-gray-600 resize-none min-h-48 ",
+          )}
+        />
+      </section>
       <button 
         onClick={handleCreateDiary}
         className={cn(
-          "p-2 bg-blue-400 m-4 rounded-lg text-white hover:bg-blue-500",
-          "transition duration-300 font-bold"
+          "px-8 py-2 m-4 text-white font-bold text-xl",
+          "bg-blue-400 rounded-lg  hover:bg-blue-500 transition duration-300"
         )}
       >
-        저장
+        저장하기
       </button>
     </div>
   );
